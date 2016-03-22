9acd0f39704dbef2fe434fa8edf737ce78055795
chenbo <smileboywtu@gmail.com>
2016-03-22 10:05:29 +0800

    add the windows mimeopen way.

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 open a file with file mimetype
 
-this library is used when I design a desktop applicaiton in a 
+this library is used when I design a desktop applicaiton in a
 company. I need to let the user choose an editor to open a file.
 
 current the library just support windows, mac, and ubuntu(linux).
 
-Be care that the lib just test on ubuntu linux. for other platform 
+Be care that the lib just test on ubuntu linux. for other platform
 not guarantee. if you meet good idea, you can tell me.
 
 # install
@@ -20,7 +20,7 @@ python setup.py install
 
 ```
 
-# usage 
+# usage
 
 it's very simple:
 
@@ -41,6 +41,19 @@ mimeopen filename
 
 **2.** you can get all apps from **/usr/share/applications**
 
-after you find the file type associations, you can find the application 
+after you find the file type associations, you can find the application
 information inside the .desktop file.
 
+## Windows
+
+on window all the program information is saved in the registry, so just check the
+registry to find out the program information.
+
+**1.** the **HKEY_CLASSES_ROOT** contains information merged from the **HKEY_CURRENT_USER**
+and **HKEY_LOCAL_MACHINE**, you can find the all the program can deal with ext.
+
+**2.** if you want to set the default program for the current user, you can set the
+program id for the UserChoice under **HKEY_CURRENT_USER\Software\Microsoft\Windows\Explorer\FileExts\ext\UserChoice**
+
+**3.** if you want to open with a program id, you can just use the program id to get
+the program shell command to open the program.
--- mimeopen/win.py
+++ mimeopen/win.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
 """module work for windows machine
@@ -12,8 +11,64 @@ author: smileboywtu
 
 """
 
+from _winreg import (
+    QueryInfoKey, OpenKey, CreateKeyEx, SetValueEx, REG_SZ, KEY_SET_VALUE,
+    HKEY_CLASSES_ROOT, HKEY_CURRENT_USER)
 
-from _winreg import OpenKey, EnumKey, EnumValue, HKEY_CLASSES_ROOT
+
+def open_with_list(ext):
+    """get the open with list from the system registry
+
+    this method find the HKEY_CLASSES_ROOT to load the application list.
+
+    params:
+        - ext file extention
+    """
+
+    root_cls = OpenKey(HKEY_CLASSES_ROOT, None)
+    key_no = QueryInfoKey(root_cls)[1]
+
+    all_keys = []
+    for index in xrange(key_no):
+        all_keys.append(EnumKey(root_cls, index))
+
+    filter_func = (
+        lambda ext_:
+            (ext_.endswith(ext) or ext_.endswith(ext.upper())) and
+            (ext_ != ext and ext_ != ext.upper()))
+
+    progids = filter(filter_func, all_keys)
+
+    return progids
+
+
+def get_prog_name(progid):
+    """use progid to find out the program name
+
+    params:
+        - progid id in registry
+    """
+    pass
+
+
+def set_user_choice(ext, progid):
+    """set the default program for the ext
+
+    params:
+        - ext file extention
+        - progid program id in regitstry
+    """
+
+    key_seq = '\\'.join([
+        'Software', 'Microsoft', 'Windows',
+        'CurrentVersion', 'Explorer', 'FileExts', ext
+        ])
+
+    user_cls = OpenKey(HKEY_CURRENT_USER, key_seq)
+
+    user_default = CreateKeyEx(user_cls, 'UserChoice', 0, KEY_SET_VALUE)
+
+    SetValueEx(user_default, 'Progid', REG_SZ, progid)
 
 
 def open_by_default(filename):
@@ -28,6 +83,27 @@ def open_by_default(filename):
     if error:
         raise AppNotFound(
             "can't found proper program to open the file: {}".format(filename))
+
+
+def open_with_prog(progid, filename):
+    """open the file with the program
+
+    params:
+        progid - program id in registry
+        filename - file name to open
+    """
+    import subprocess
+
+    key_seq = '\\'.join([
+        progid, 'Shell', 'Open', 'Command'
+    ])
+
+    prog_cls = OpenKey(HKEY_CLASSES_ROOT, key_seq)
+
+    command = EnumValue(prog_cls, 0)[2]
+
+    # execute the command
+    pass
 
 
 def mimeopen(filename):
