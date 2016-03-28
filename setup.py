f8d7a6d91b487c220e5805795ba836819ca1547a
chenbo <smileboywtu@gmail.com>
2016-03-28 11:36:59 +0800

    add the install script

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='mimeopen',
+    version='1.0',
+    packages=['mimeopen', ],
+    description='get open with list for a file',
+    author='smileboywtu',
+    author_email="smileboywtu@gmail.com",
+    url='http://smileboywtu.github.io/articles/2016/03/13/python-get-system-default-app-related-a-mime-type.html',
+    license='GPL3',
+    required=[],
+    entry_points={
+        'console_scripts': [
+
+        ]
+    }
+)
