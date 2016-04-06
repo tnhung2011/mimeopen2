12cc67d271b4d070371de7ba46d2ae95c2f99c28
chenbo <smileboywtu@gmail.com>
2016-04-06 18:06:00 +0800

    window bug fix and make it work correctly

--- mimeopen/win.py
+++ mimeopen/win.py
@@ -29,16 +29,19 @@ def open_with_list(ext):
     key = OpenKey(HKEY_CLASSES_ROOT, None)
     key_no = QueryInfoKey(key)[0]
 
+    # get all sub keys
     all_keys = []
     for index in xrange(key_no):
         all_keys.append(EnumKey(key, index))
 
-    filter_func = (
-        lambda ext_:
-            (ext_.endswith(ext) or ext_.endswith(ext.upper())) and
-            (ext_ != ext and ext_ != ext.upper()))
-
-    progids = filter(filter_func, all_keys)
+    # check if the default program already in list
+    progids = []
+    key = OpenKey(HKEY_CLASSES_ROOT, ext)
+    key_no = QueryInfoKey(key)[0]
+    for index in xrange(key_no):
+        _id = EnumKey(key, index)
+        if _id in all_keys:
+            progids.append(_id)
 
     return progids
 
@@ -75,14 +78,48 @@ def get_prog_command(progid):
     return open_command, edit_command
 
 
+def search_name(locals, prog_path):
+    """use the prog_path to find out the program display name
+
+    locals: -- (main_key, sub_key)
+    prog_path: -- program execute path
+
+    return prog_name
+
+    """
+
+    for local in locals:
+
+        key = OpenKey(*local)
+        key_no = QueryInfoKey(key)[0]
+
+        for index in xrange(key_no):
+
+            key_name = EnumKey(key, index)
+            app_key = OpenKey(key, key_name)
+
+            for value_name in ('InstallLocation', 'LocationRoot', 'UninstallString'):
+                try:
+                    value_data = QueryValueEx(app_key, value_name)[0]
+                    if value_data and (value_data.startswith(prog_path) or prog_path.startswith(value_data)):
+                        prog_name = QueryValueEx(app_key, 'DisplayName')[0]
+                        return prog_name
+                except WindowsError:
+                    pass
+
+    return None
+
+
 def get_prog_name(progid):
     """use progid to find out the program name
 
     get this information from the uninstall list
 
-    HKEY_LOCAL_MACHINE\SOFTWARE\
-    Wow6432Node\Microsoft\Windows\
-    CurrentVersion\Uninstall
+    HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
+
+    HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall
+
+    HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall
 
     params:
         - program id in registry
@@ -90,47 +127,41 @@ def get_prog_name(progid):
     open_command, edit_command = get_prog_command(progid)
 
     prog_path = None
-    prog_name = None
 
     if open_command is not None:
         import re
-        match = re.search(r'\"(.+)\\.+\.exe\"', open_command)
+        match = re.search(u'\"(.+)\\\\\w+\.exe\"\s',
+                          open_command, flags=re.IGNORECASE)
         if match:
             prog_path = match.group(1)
 
     if not prog_path:
         return None
 
+    locals = []
+
+    main_key = HKEY_LOCAL_MACHINE
     sub_key = '\\'.join([
         'SOFTWARE', 'Wow6432Node', 'Microsoft', 'Windows',
         'CurrentVersion', 'Uninstall'
     ])
+    locals.append((main_key, sub_key))
 
-    key = OpenKey(HKEY_LOCAL_MACHINE, sub_key)
-    key_no = QueryInfoKey(key)[0]
+    main_key = HKEY_LOCAL_MACHINE
+    sub_key = '\\'.join([
+        'Software', 'Microsoft', 'Windows',
+        'CurrentVersion', 'Uninstall'
+    ])
+    locals.append((main_key, sub_key))
 
-    for index in xrange(key_no):
+    main_key = HKEY_CURRENT_USER
+    sub_key = '\\'.join([
+        'SOFTWARE', 'Microsoft', 'Windows',
+        'CurrentVersion', 'Uninstall'
+    ])
+    locals.append((main_key, sub_key))
 
-        key_name = EnumKey(key, index)
-        app_key = OpenKey(key, key_name)
-
-        try:
-            install_path = QueryValueEx(app_key, 'InstallLocation')[0]
-            if prog_path in install_path:
-                prog_name = QueryValueEx(app_key, 'DisplayName')[0]
-                return prog_name
-        except WindowsError:
-            pass
-
-        try:
-            remove_path = QueryValueEx(app_key, 'UninstallString')[0]
-            if prog_path in remove_path:
-                prog_name = QueryValueEx(app_key, 'DisplayName')[0]
-                return prog_name
-        except WindowsError:
-            pass
-
-    return None
+    return search_name(locals, prog_path)
 
 
 def set_user_editor(ext, progid, command):
@@ -171,7 +202,7 @@ def set_user_choice(ext, progid):
     key_seq = '\\'.join([
         'Software', 'Microsoft', 'Windows',
         'CurrentVersion', 'Explorer', 'FileExts', ext, 'UserChoice'
-        ])
+    ])
 
     user_default = CreateKeyEx(HKEY_CURRENT_USER, key_seq, 0, KEY_SET_VALUE)
 
