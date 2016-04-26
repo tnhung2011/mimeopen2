4630773a2ff2e5d945d3d5cc859c78194c4f281e
chenbo <smileboywtu@gmail.com>
2016-04-26 18:57:59 +0800

    update the win version of find a program

--- mimeopen/win.py
+++ mimeopen/win.py
@@ -13,45 +13,106 @@ author: smileboywtu
 
 from _winreg import (
     QueryInfoKey, QueryValueEx, OpenKey, EnumKey, CreateKeyEx, SetValueEx,
-    REG_SZ, KEY_SET_VALUE, HKEY_CLASSES_ROOT, HKEY_CURRENT_USER,
-    HKEY_LOCAL_MACHINE)
+    ExpandEnvironmentStrings, REG_SZ, KEY_SET_VALUE,
+    HKEY_CLASSES_ROOT, HKEY_CURRENT_USER, HKEY_LOCAL_MACHINE
+)
 
 
-def open_with_list(ext):
-    """get the open with list from the system registry
-
-    this method find the HKEY_CLASSES_ROOT to load the application list.
-
-    params:
-        - ext file extention
+def get_command(key, sub_key):
+    """get the open and edit command under the key
+    @params:
+        key -- registry key
+        sub_key -- sub key
+    @return:
+        command
     """
+    command = None
+    try:
+        key = OpenKey(key, sub_key)
+        command = QueryValueEx(key, None)[0]
+    except OSError:
+        pass
+    return command
 
-    key = OpenKey(HKEY_CLASSES_ROOT, None)
+
+def get_exe_command(exe):
+    """get the application run command
+
+    @params:
+        exe -- application
+    @return:
+        command -- command
+    """
+    sub_key = '\\'.join([
+        'Applications', exe
+    ])
+
+    try:
+        key = OpenKey(HKEY_CLASSES_ROOT, sub_key)
+    except WindowsError:
+        return None
+
+    sub_key = '\\'.join([
+        'shell', 'edit', 'command'
+    ])
+    edit_command = get_command(key, sub_key)
+    sub_key = '\\'.join([
+        'shell', 'open', 'command'
+    ])
+    open_command = get_command(key, sub_key)
+
+    if not (open_command or edit_command):
+        return None
+
+    return edit_command or open_command
+
+
+def get_wildcard_program():
+    """find all exe inside the wildcard key
+    @params:
+        None
+    @return
+        [(name, command)] -- program name and command
+    """
+    import re
+
+    sub_key = r'\*\OpenWithList'
+    key = OpenKey(HKEY_CLASSES_ROOT, sub_key)
+
     key_no = QueryInfoKey(key)[0]
 
-    # get all sub keys
-    all_keys = []
+    exes = []
     for index in xrange(key_no):
-        all_keys.append(EnumKey(key, index))
+        exe = EnumKey(key, index)
+        if exe.lower().endswith('.exe'):
+            command = get_exe_command(exe)
+            if not command:
+                continue
+            match = re.search(u'.+\\\\(\w+)\.exe', command,  flags=re.IGNORECASE)
+            if match:
+                name = match.group(1)
+                progid = '.'.join([name, 'edo'])
+                exes.append((name, progid, command))
+        else:
+            sub_key = '\\'.join([
+                exe, 'shell', 'edit', 'command'
+            ])
+            edit_command = get_command(key, sub_key)
+            sub_key = '\\'.join([
+                exe, 'shell', 'open', 'command'
+            ])
+            open_command = get_command(key, sub_key)
 
-    # check if the default program already in list
-    progids = []
-    key = OpenKey(HKEY_CLASSES_ROOT, ext)
-    progids.append(QueryValueEx(key, None)[0])
-    key_no = QueryInfoKey(key)[0]
-    for index in xrange(key_no):
-        _id = EnumKey(key, index)
-        if _id in all_keys:
-            progids.append(_id)
+            if not (open_command or edit_command):
+                continue
 
-    filter_func = (
-        lambda ext_:
-        (ext_.endswith(ext) or ext_.endswith(ext.upper())) and
-        (ext_ != ext and ext_ != ext.upper()))
-
-    progids.extend(filter(filter_func, all_keys))
-
-    return progids
+            command = edit_command or open_command
+            match = re.search(u'.+\\\\(\w+)\.exe', command,  flags=re.IGNORECASE)
+            if match:
+                name = match.group(1)
+                progid = '.'.join([name, 'edo'])
+                exes.append((name, progid, command))
+    return exes
 
 
 def get_prog_command(progid):
@@ -83,7 +144,142 @@ def get_prog_command(progid):
     except WindowsError:
         pass
 
-    return open_command, edit_command
+    if not (open_command or edit_command):
+        return None
+
+    return edit_command or open_command
+
+
+def open_program(ext):
+    """get the open with list for the ext
+
+    1. open with list
+
+    2. open with progids
+
+    @params:
+        ext -- file extention
+    @return
+        (name, progid, command) -- program name and program command
+    """
+    import re
+
+    # get the open with list
+    sub_key = '\\'.join([
+        ext, 'OpenWithList'
+    ])
+
+    try:
+        key = OpenKey(HKEY_CLASSES_ROOT, sub_key)
+    except WindowsError:
+        return None
+
+    key_no = QueryInfoKey(key)[0]
+
+    exes = []
+    for index in xrange(key_no):
+        exe = EnumKey(key, index)
+        if exe.lower().endswith('.exe'):
+            command = get_exe_command(exe)
+            if not command:
+                continue
+            match = re.search(u'.+\\\\(\w+)\.exe', command,  flags=re.IGNORECASE)
+            if match:
+                name = match.group(1)
+                progid = '.'.join([name, 'edo'])
+                exes.append((name, progid, command))
+        else:
+            sub_key = '\\'.join([
+                exe, 'shell', 'edit', 'command'
+            ])
+            edit_command = get_command(key, sub_key)
+            sub_key = '\\'.join([
+                exe, 'shell', 'open', 'command'
+            ])
+            open_command = get_command(key, sub_key)
+
+            if not (open_command or edit_command):
+                continue
+
+            command = edit_command or open_command
+            match = re.search(u'.+\\\\(\w+)\.exe', command,  flags=re.IGNORECASE)
+            if match:
+                name = match.group(1)
+                progid = '.'.join([name, 'edo'])
+                exes.append((name, progid, command))
+    return exes
+
+
+def edit_program(ext):
+    """get the edit progid
+    @params:
+        ext -- file extention
+    @return
+        (name, command) -- program name and program command
+    """
+    import re
+
+    key = OpenKey(HKEY_CLASSES_ROOT, None)
+    key_no = QueryInfoKey(key)[0]
+
+    # get all sub keys
+    all_keys = []
+    for index in xrange(key_no):
+        all_keys.append(EnumKey(key, index))
+
+    # check if the default program already in list
+    key = OpenKey(HKEY_CLASSES_ROOT, ext)
+    progids = [QueryValueEx(key, None)[0]]
+    key_no = QueryInfoKey(key)[0]
+    for index in xrange(key_no):
+        _id = EnumKey(key, index)
+        if _id in all_keys:
+            progids.append(_id)
+
+    key = OpenKey(HKEY_CLASSES_ROOT, None)
+    key_no = QueryInfoKey(key)[0]
+
+    filter_func = (
+        lambda ext_:
+            (ext_.endswith(ext) or ext_.endswith(ext.upper())) and
+            (ext_ != ext and ext_ != ext.upper()))
+    progids.extend(filter(filter_func, all_keys))
+
+    exes = []
+    for progid in progids:
+        name = get_prog_name(progid)
+        command = get_prog_command(progid)
+        if name and command:
+            exes.append((name, progid, command))
+        if command and not name:
+            match = re.search(u'.+\\\\(\w+)\.exe', command,  flags=re.IGNORECASE)
+            if match:
+                name = match.group(1)
+                exes.append((name, progid, command))
+    return exes
+
+
+def assoc_ext_programs(ext):
+    """get all the programs can deal with the ext
+
+    params:
+        ext -- file extention
+    return:
+        (name, path) -- program name and run time command
+    """
+    programs = []
+
+    # try to get edit_program
+    programs.extend(edit_program(ext))
+
+    # try to get open_program
+    if not programs:
+        programs.extend(open_program(ext))
+
+    # append wildcard
+    programs.extend(get_wildcard_program())
+
+    return list(set(programs))
 
 
 def search_name(locals, prog_path):
@@ -132,14 +328,14 @@ def get_prog_name(progid):
     params:
         - program id in registry
     """
-    open_command, edit_command = get_prog_command(progid)
+    command = get_prog_command(progid)
 
     prog_path = None
 
-    if open_command is not None:
+    if command:
         import re
         match = re.search(u'\"(.+)\\\\\w+\.exe\"\s',
-                          open_command, flags=re.IGNORECASE)
+                          command, flags=re.IGNORECASE)
         if match:
             prog_path = match.group(1)
 
@@ -182,6 +378,11 @@ def set_user_editor(ext, progid, command):
         progid - progid
         progpath - program execute path
     """
+
+    # must use the expand winreg string, if not, you may not have access
+    # to the app and you need to use REG_EXPAND_SZ value type
+    command = ExpandEnvironmentStrings(command)
+
     sub_key = '\\'.join([
         'Software', 'Classes', ext
     ])
@@ -224,8 +425,8 @@ def query_user_choice(progs):
         progs - program list
     """
     print "Available Program: "
-    for index, prog in enumerate(progs):
-        print '\t', index, ': ', prog[1]
+    for index, name, progid, command in enumerate(progs):
+        print '\t', index, ': ', name
 
     choice = None
     max_size = len(progs)
@@ -264,18 +465,12 @@ def mimeopen(filename):
     if match:
         ext = match.group(1)
 
-    progids = open_with_list(ext)
-
-    proginfo = []
-    for pid in progids:
-        progname = get_prog_name(pid)
-        if progname:
-            proginfo.append((pid, progname))
+    proginfo = assoc_ext_programs(ext)
 
     if proginfo:
         choice = query_user_choice(proginfo)
-        progcommand = get_prog_command(proginfo[choice][0])
-        match = re.search(r'\"(.+\.exe)\"', progcommand[0])
+        command = ExpandEnvironmentStrings(proginfo[choice][2])
+        match = re.search(r'\"(.+\.exe)\"', command[0])
         error = execute_program(match.group(1), filename)
         if error:
             print 'Error happens when execute the program.'
