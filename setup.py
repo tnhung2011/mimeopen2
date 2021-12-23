44b00368646519ffed8ed0d0f210de4f537b45cc
IAmOlive <nhathungtran2011@gmail.com>
2021-12-23 08:34:51 +0700

    Update version

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import setup
 
 setup(
     name='mimeopen',
-    version='1.0',
-    packages=['mimeopen', ],
+    version='2.0',
+    packages=['mimeopen'],
     description='get open with list for a file',
     author='smileboywtu',
     author_email="smileboywtu@gmail.com",
     url='http://smileboywtu.github.io/articles/2016/03/13/python-get-system-default-app-related-a-mime-type.html',
     license='GPL3',
-    required=[],
+    required=None,
     entry_points={
         'console_scripts': [
-            'mimeopen = mimeopen.__init__:main',   
+            'mimeopen = mimeopen.__init__:main',
         ]
     }
 )
